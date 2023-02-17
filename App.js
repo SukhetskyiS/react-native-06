74a0cc829ec85a5e37bd213cee6f87bf8e135928
SukhetskyiS <medpredin@gmail.com>
2023-02-17 17:58:59 +0200

    555

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
-import { store, persistor } from './redux/store'
-import { Provider } from 'react-redux'
-import { DefaultApp } from './Screens/DefaultApp/DefaultApp'
-import { PersistGate } from 'redux-persist/integration/react'
-import { useEffect } from 'react'
+import { store, persistor } from "./redux/store";
+import { Provider } from "react-redux";
+import { DefaultApp } from "./Screens/DefaultApp/DefaultApp";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function App() {
-    return (
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <DefaultApp />
-            </PersistGate>
-        </Provider>
-    )
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <DefaultApp />
+      </PersistGate>
+    </Provider>
+  );
 }
--- Screens/Posts/databaseRequests.js
+++ Screens/Posts/databaseRequests.js
@@ -1,46 +1,63 @@
-import { Platform } from 'react-native'
-import uuid from 'react-native-uuid'
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
-import { addDoc, collection, getDocs } from 'firebase/firestore'
-import exportFirebase from '../../firebase/config'
+import { Platform } from "react-native";
+import uuid from "react-native-uuid";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
+import {
+  addDoc,
+  collection,
+  getDocs,
+} from "firebase/firestore";
+import exportFirebase from "../../firebase/config";
 
-const { storage, db } = exportFirebase
+const { storage, db } = exportFirebase;
 
-export const uploadPhotoToDb = async (photo, setPhotoDb) => {
-    const uniquePostId = uuid.v4()
-    const { uri } = photo
-    const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri
-    const storageRef = ref(storage, `images/${uniquePostId}`)
+export const uploadPhotoToDb = async (
+  photo,
+  setPhotoDb
+) => {
+  const uniquePostId = uuid.v4();
+  const { uri } = photo;
+  const uploadUri =
+    Platform.OS === "ios"
+      ? uri.replace("file://", "")
+      : uri;
+  const storageRef = ref(storage, `images/${uniquePostId}`);
 
-    const response = await fetch(uploadUri)
-    const file = await response.blob()
+  const response = await fetch(uploadUri);
+  const file = await response.blob();
 
-    const metadata = {
-        contentType: 'image/jpeg',
-    }
+  const metadata = {
+    contentType: "image/jpeg",
+  };
 
-    await uploadBytes(storageRef, file, metadata)
+  await uploadBytes(storageRef, file, metadata);
 
-    const getPhotoUrl = await getDownloadURL(
-        ref(storage, `images/${uniquePostId}`)
-    )
-    if (getPhotoUrl !== '') {
-        setPhotoDb(getPhotoUrl)
-    }
-}
+  const getPhotoUrl = await getDownloadURL(
+    ref(storage, `images/${uniquePostId}`)
+  );
+  if (getPhotoUrl !== "") {
+    setPhotoDb(getPhotoUrl);
+  }
+};
 
-export const uploadPostToDb = async (photoDb, geolocation, state, user) => {
-    try {
-        const docRef = await addDoc(collection(db, 'posts'), {
-            photo: photoDb,
-            geolocation,
-            state,
-            uid: user.uid,
-            nickName: user.displayName,
-        })
-
-        // console.log('Document written with ID: ', docRef.id)
-    } catch (e) {
-        console.error('Error adding document: ', e)
-    }
-}
+export const uploadPostToDb = async (
+  photoDb,
+  geolocation,
+  state,
+  user
+) => {
+  try {
+    const docRef = await addDoc(collection(db, "posts"), {
+      photo: photoDb,
+      geolocation,
+      state,
+      uid: user.uid,
+      nickName: user.displayName,
+    });
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
